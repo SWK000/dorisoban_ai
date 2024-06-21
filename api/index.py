1c2bb1c99f36d8567ffa9826c3a98b886efb9860
SWKing <79729646+SWK000@users.noreply.github.com>
2024-06-22 02:25:10 +0900

    Update index.py f

--- api/index.py
+++ api/index.py
@@ -2,15 +2,20 @@ from flask import Flask, request, jsonify
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_watson.natural_language_understanding_v1 import Features, KeywordsOptions
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
 
 app = Flask(__name__)
 
-authenticator = IAMAuthenticator('GFqSHawAvnoLmgTyS4YG7geo8Ob3h8KLBZLRFtPKTKj7')
+# 환경 변수에서 IBM Watson API 키와 서비스 URL 가져오기
+IBM_API_KEY = 'GFqSHawAvnoLmgTyS4YG7geo8Ob3h8KLBZLRFtPKTKj7'
+IBM_SERVICE_URL = 'https://api.au-syd.natural-language-understanding.watson.cloud.ibm.com/instances/b4542792-15e1-4cee-8e98-0ed255cd2a74'
+
+authenticator = IAMAuthenticator(IBM_API_KEY)
 nlu = NaturalLanguageUnderstandingV1(
     version='2021-08-01',
     authenticator=authenticator
 )
-nlu.set_service_url('https://api.au-syd.natural-language-understanding.watson.cloud.ibm.com/instances/b4542792-15e1-4cee-8e98-0ed255cd2a74')
+nlu.set_service_url(IBM_SERVICE_URL)
 
 @app.route('/api/webhook', methods=['POST'])
 def webhook():
@@ -22,9 +27,9 @@ def webhook():
         features=Features(keywords=KeywordsOptions())
     ).get_result()
 
-    keywords = [keyword['text'] for keyword in response['keywords']]
+    keywords = [keyword['text'] for keyword in response.get('keywords', [])]
     reply = f"You mentioned: {', '.join(keywords)}"
-    
+
     return jsonify({
         "version": "2.0",
         "template": {
@@ -39,5 +44,4 @@ def webhook():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
-
+    app.run(host='0.0.0.0', port=8000)
